5c10e3e21c521c8ff983765adce74b03806d448d
Sebastian Sanitz <sebastian@sanitz.net>
2020-12-01 20:53:25 +0100

    Clean up

--- day01/day1.test.js
+++ day01/day1.test.js
@@ -1,5 +1,5 @@
 /*
-https://adventofcode.com/2020/day/1
+https://adventofcode.com/2020/day/1 - Part 1
 
 Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
 
@@ -17,42 +17,40 @@ Of course, your expense report is much larger. Find the two entries that sum to 
 */
 
 function productOfTwo(sum, arr) {
-  for (let num1 of arr) {
-    if (arr.includes(sum - num1)) {
-      return (sum - num1) * num1;
+  for (const num1 of arr) {
+    const num2 = sum - num1;
+    if (arr.includes(num2)) {
+      return num1 * num2;
     }
   }
-  return undefined;
 }
 
-test("Example Day 01 empty", () => {
+test("Part 1 with no result", () => {
   expect(productOfTwo(3, [1, 3])).toBeUndefined();
 });
 
-test("Example Day 01 simple", () => {
+test("Part 1 with a simple result", () => {
   expect(productOfTwo(3 + 4, [3, 4])).toEqual(3 * 4);
 });
 
-test("Example Day 01 given", () => {
-  let arr = [1721, 979, 366, 299, 675, 1456];
+test("Part 1 with the example given", () => {
+  const arr = [1721, 979, 366, 299, 675, 1456];
   expect(productOfTwo(2020, arr)).toEqual(1721 * 299);
 });
 
 const fs = require("fs");
 
 function entriesDay01() {
-  let buffer = fs.readFileSync("input.txt", "utf-8").split("\n");
+  const buffer = fs.readFileSync("input.txt", "utf-8").split("\n");
   return buffer.map((x) => Number.parseInt(x));
 }
 
-test("Result Day 01 Part 01", () => {
+test("Result Part 1", () => {
   expect(productOfTwo(2020, entriesDay01())).toEqual(787776);
 });
 
-
 /*
-
-# Part 2
+https://adventofcode.com/2020/day/1 - Part 1
 
 The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
 
@@ -62,30 +60,29 @@ In your expense report, what is the product of the three entries that sum to 202
 */
 
 function productOfThree(sum, arr) {
-  for (let num1 of arr) {
-    for (let num2 of arr) {
-      let num3 = sum - num1 - num2;
+  for (const num1 of arr) {
+    for (const num2 of arr) {
+      const num3 = sum - num1 - num2;
       if (arr.includes(num3)) {
         return BigInt(num3) * BigInt(num1) * BigInt(num2);
       }
     }
   }
-  return undefined;
 }
 
-test("With  three is empty", () => {
+test("Part 2 with no result", () => {
   expect(productOfThree(42, [1, 3, 7])).toBeUndefined();
 });
 
-test("Example Day 01 with three simple ", () => {
+test("Part 2 with simple result ", () => {
   expect(productOfThree(3 + 4 + 5, [3, 4, 5])).toEqual(BigInt(3 * 4 * 5));
 });
 
-test("Example Day 01 given", () => {
+test("Part 2 with example given", () => {
   let arr = [1721, 979, 366, 299, 675, 1456];
   expect(productOfThree(2020, arr)).toEqual(241861950n);
 });
 
-test("Result Day 01 Part 02", () => {
+test("Result Part 2", () => {
   expect(productOfThree(2020, entriesDay01())).toEqual(262738554n);
 });
