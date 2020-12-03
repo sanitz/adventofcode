40dd3bc015490a65273442f0c216029971ae263b
Sebastian Sanitz <sebastian@sanitz.net>
2020-12-03 10:23:24 +0100

    Day 3 Part 2

--- day03/day3.test.js
+++ day03/day3.test.js
@@ -52,32 +52,18 @@ In this example, traversing the map using this slope would cause you to encounte
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
 **/
 
-const given = [
-  "..##.......",
-  "#...#...#..",
-  ".#....#..#.",
-  "..#.#...#.#",
-  ".#...##..#.",
-  "..#.##.....",
-  ".#.#.#....#",
-  ".#........#",
-  "#.##...#...",
-  "#...##....#",
-  ".#..#...#.#",
-];
+const isTree = (line, col) => line[col % line.length] === "#";
 
-function isTree(line, row) {
-  return line[row % line.length] === "#";
-}
+const defaultSlope = { right: 3, down: 1 };
 
-function countTrees(lines) {
+function countTrees(grid, slope = defaultSlope) {
   let result = 0;
   let row = 0;
-  for (const line of lines) {
-    if (isTree(line, row)) {
-      result = result + 1;
-    }
-    row = row + 3;
+  let col = 0;
+  while (row < grid.length) {
+    if (isTree(grid[row], col)) result = result + 1;
+    col = col + slope.right;
+    row = row + slope.down;
   }
   return result;
 }
@@ -97,34 +83,74 @@ test("Part 1 test for tree in line", () => {
   expect(isTree(".#....#..#.", 6)).toBeTruthy();
 });
 
+const exampleGridGiven = [
+  "..##.......",
+  "#...#...#..",
+  ".#....#..#.",
+  "..#.#...#.#",
+  ".#...##..#.",
+  "..#.##.....",
+  ".#.#.#....#",
+  ".#........#",
+  "#.##...#...",
+  "#...##....#",
+  ".#..#...#.#",
+];
+
 test("Part 1 count trees", () => {
   expect(countTrees([])).toEqual(0);
   expect(countTrees(["..##......."])).toEqual(0);
-  expect(
-    countTrees([
-      "..##.......",
-      "#...#...#..",
-      ".#....#..#.",
-      "..#.#...#.#",
-      ".#...##..#.",
-      "..#.##.....",
-      ".#.#.#....#",
-      ".#........#",
-      "#.##...#...",
-      "#...##....#",
-      ".#..#...#.#",
-    ])
-  ).toEqual(7);
+  expect(countTrees(exampleGridGiven)).toEqual(7);
 });
 
 const fs = require("fs");
-
-function entriesDay03() {
-  return fs.readFileSync(__dirname + "/input.txt", "utf-8").split("\n");
-}
+const gridDay03 = fs
+  .readFileSync(__dirname + "/input.txt", "utf-8")
+  .split("\n");
 
 test("Part 1 result", () => {
-  const entries = entriesDay03();
-  expect(entries[0]).toEqual("....#.#..#.#.#.#......#....##.#");
-  expect(countTrees(entriesDay03())).toEqual(159);
+  expect(countTrees(gridDay03)).toEqual(159);
+});
+
+/*
+--- Part Two ---
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+Right 1, down 1.
+Right 3, down 1. (This is the slope you already checked.)
+Right 5, down 1.
+Right 7, down 1.
+Right 1, down 2.
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+*/
+
+const slopes = [
+  { right: 1, down: 1 },
+  { right: 3, down: 1 },
+  { right: 5, down: 1 },
+  { right: 7, down: 1 },
+  { right: 1, down: 2 },
+];
+
+test("Part 2 results given slopes", () => {
+  expect(countTrees(exampleGridGiven, slopes[0])).toEqual(2);
+  expect(countTrees(exampleGridGiven, slopes[1])).toEqual(7);
+  expect(countTrees(exampleGridGiven, slopes[2])).toEqual(3);
+  expect(countTrees(exampleGridGiven, slopes[3])).toEqual(4);
+  expect(countTrees(exampleGridGiven, slopes[4])).toEqual(2);
+});
+
+const sumOfSlopes = (grid, slopes) =>
+  slopes.reduce((val, slope) => countTrees(grid, slope) * val, 1);
+
+test("Part 2 sum of given slopes", () => {
+  expect(sumOfSlopes(exampleGridGiven, slopes)).toEqual(336);
+});
+
+test("Part 2 result", () => {
+  expect(sumOfSlopes(gridDay03, slopes)).toEqual(6419669520);
 });
